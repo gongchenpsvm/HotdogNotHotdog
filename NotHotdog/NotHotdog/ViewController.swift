4ab00fadb6d21ca8793e1033cc8b0a265ebc6073
gongchen <gongchen@usc.edu>
2017-09-21 11:30:29 -0700

    bluemix returns recognition result

--- NotHotdog/NotHotdog/ViewController.swift
+++ NotHotdog/NotHotdog/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import VisualRecognitionV3
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let apikey = "9c33c4e7c84899c64f370f15b485fcfcf3c0351d"
+    let version = "2017-09-21"
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet  var cameraButton: UIBarButtonItem!
@@ -35,6 +38,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
             //Dismiss the imagepicker used earlier
             imagePicker.dismiss(animated: true, completion: nil)
+            
+            let visualRecognition = VisualRecognition(apiKey: apikey, version: version)
+            
+            let imageData = UIImageJPEGRepresentation(image, 0.01)
+            
+            let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            
+            let fileURL = documentsURL.appendingPathComponent("tempImage.jpg")
+            
+            try? imageData?.write(to: fileURL, options: [])
+            
+            visualRecognition.classify(imageFile: fileURL, success: { (classifiedImages) in
+                print(classifiedImages)
+            })
         } else {//Not true
             print("Error: picking image")
         }
